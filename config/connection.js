c68465be082546228b4339f6e5feb8df92573b68
Daniel Murray <danielmurray@Daniels-MacBook-Pro.local>
2018-03-05 14:48:47 -0800

    update config

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var connection;
 
 if(process.env.JASWDB_URL){
 
-	connection = mysql.createConnection(process.env.JASWDB_URL);
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 } else {
 	connection = mysql.createConnection({
