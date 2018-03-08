8453648d7fff3b6ef4788259081a98d0f024a1c6
Frank Valdez <fvaldez421@gmail.com>
2018-03-08 13:07:36 -0800

    working post function

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,12 @@
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 var db = require("../models");
+var path = require("path");
 
-
+module.exports = function(app) {
 app.post("/api/profile", function(req, res) {
 	console.log(req.body);
 
 });
 
+}
