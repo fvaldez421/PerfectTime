11d7322d5bd4490c0f6a03bd0762b32fbf0eaae2
Daniel Murray <danielgrmurray@gmail.com>
2018-03-08 14:44:04 -0800

    charts data transfer

---
+++ models/general.js
@@ -0,0 +1,27 @@
+
+module.exports = function(sequelize, DataTypes) {
+	var General = sequelize.define("General", {
+		fdLastPeriod: {
+			type: DataTypes.STRING,
+			allowNull: false
+			
+		},
+		cycleLength: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+		
+	}, {
+		timestamps: false
+	});
+
+	General.associate = function(models) {
+		General.belongsTo(models.Member, {
+			foreignKey: {
+				allowNull: false
+			}
+		});
+	};
+
+	return General;
+};
