39f8adc3c751eeddf91e7400f2c89d5c8b751974
Frank Valdez <fvaldez421@gmail.com>
2018-03-08 18:17:49 -0800

    working db

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,27 +2,29 @@
 
 $(document).ready(function() {
 
-	var googleId = $("#googleId").html()
 
-	var name = $("#q1").val().trim();
-	var age = $("#q2").val().trim();
-	var height = $("#q3").val().trim();
-	var weight = $("#q4").val().trim();
-	var gender = $("#q5").val().trim();
-	var conceive = $("#q6").val();
-
-	var info = {
-		googleId: googleId,
-	    nickName: name,
-	   	age: age,
-	    height: height,
-	   	weight: weight,
-	    gender: gender,
-	    conception: conceive
-	};
 	console.log("Im loaded");
 
 	$("#submitButton").on("click", function(event) {
+		var googleId = $("#googleId").html()
+
+		var name = $("#q1").val().trim();
+		var age = $("#q2").val().trim();
+		var height = $("#q3").val().trim();
+		var weight = $("#q4").val().trim();
+		var gender = $("#q5").val().trim();
+		var conceive = $("#q6").val();
+
+		var info = {
+			googleId: googleId,
+		    nickName: name,
+		   	age: age,
+		    height: height,
+		   	weight: weight,
+		    gender: gender,
+		    conception: conceive
+		};
+		
 		event.preventDefault();
 		console.log(info.nickName);
 		console.log("name here: " + name);
